b5cdf1f333566205e22ae2550b980d3c970be14d
fihryal faqiy <fihryalfaqiy@fihryals-MacBook-Pro.local>
2021-09-10 15:01:36 +0700

    Initial commit

---
+++ app/src/main/java/com/fihryal/dashboardislami/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.fihryal.dashboardislami
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
+
+// identifier = nama
+// menggunakan huruf kecil (snake case)
+// tidak boleh menggunkan spas atau karakter khusus
+// diawali dengan huruf
+// menggunakn gaya snake case
